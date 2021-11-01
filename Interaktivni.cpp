a5f6f4d42bdbdf7f7b221448a8da94614b9a5194
Milan Keršláger <milan.kerslager@pslib.cz>
2021-11-01 14:29:59 +0100

    Add project files.

---
+++ Interaktivni.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+	int vstup, a, b, c;
+	printf("Vyber si:\n");
+	printf("1) trojuhelnik\n");
+	printf("2) obdelnik\n");
+	printf("3) kruznice\n");
+	scanf("%d", &vstup);
+	if (vstup == 1) {
+		// resime trojuhelnik
+
+	} else if (vstup == 2) {
+		// resime obdelnik
+
+	} else {
+		// resime kruznici
+		printf("Zadej polomer kruznice: ");
+		scanf("%d", &a);
+		printf("Obvod: %f\n", 3.14 * 2 * a);
+		printf("Obsah: %f\n", 3.14 * a * a);
+	}
+}
